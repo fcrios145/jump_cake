440ca689534fba920b07bf1c9b8f03b9c92ff56f
fcrios145 <fcrios145@hotmail.com>
2014-03-19 12:19:26 +0000

    View para noticias en controlerr y vistas

--- app/Controller/NewsController.php
+++ app/Controller/NewsController.php
@@ -6,9 +6,17 @@ class NewsController extends AppController {
         $this->set('news', $this->News->find('all'));
     }
 
-    public function admin_view() {
-
+    public function admin_view($id) {
+        if (!$id) {
+            throw new NotFoundException(__('Noticia no encontrada'));
+        }
+        $noticia = $this->News->findById($id);
+        if(!$noticia) {
+            throw new NotFoundException(__('Noticia no encontrada'));
+        }
+        $this->set('news',$noticia);
     }
+
 }
 
 ?>
---
+++ app/View/News/admin_view.ctp
@@ -0,0 +1,5 @@
+<h1><?php echo h($noticia['News']['titulo']); ?></h1>
+
+<p><small>Created: <?php echo $noticia['News']['created']; ?></small></p>
+
+<p><?php echo h($noticia['News']['body']); ?></p>
